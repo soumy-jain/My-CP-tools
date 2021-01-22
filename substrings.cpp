52f284d60fb462cfde18bd9e10fb454e603278cc
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-01-22 15:13:15 +0530

    Update substrings.cpp

--- substrings.cpp
+++ substrings.cpp
@@ -2,9 +2,11 @@ void get(string &s)
 {
     for(ll i=0;i<s.length();i++)
     {
-        for(ll j=1;j<=s.length()-i;j++)
+        string t="";
+        for(ll j=i;j<s.length();j++)
         {
-            string t=s.substr(i,j);
+            t+=s[j];
+            
         }
     }
 }
