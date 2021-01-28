b5a02f0bd575176504772b0a85b8703554a92673
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-01-28 19:19:33 +0530

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,20 +1,20 @@
-ll N=1000005;
-vector <bool> prime;
+ll N = 1000005;
+vector<bool> prime;
 
 void sieve()
 {
-    prime[0]=prime[1]=true;
-    for(ll i=2;i*i<=N;i++)
-    {
-        if(prime[i]==false)
-        {
-            for(ll j=i*i;j<=N;j+=i)
-            {
-                prime[j]=true;
-            }
-        }
-    }
+	prime[0] = prime[1] = false;
+	for (ll i = 2; i * i < N; i++)
+	{
+		if (prime[i] == true)
+		{
+			for (ll j = i * i; j < N; j += i)
+			{
+				prime[j] = false;
+			}
+		}
+	}
 }
 
-prime.resize(N+1,false);
+prime.resize(N, true);
 sieve();
