56480ecbc6cda4f4acc929b18fd15e2b97dad399
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-02-10 22:30:02 +0530

    Add files via upload

---
+++ segmentedSieve.cpp
@@ -0,0 +1,23 @@
+vector<bool> sSieve(long long L, long long R)
+{
+    long long lim = sqrt(R);
+    vector<bool> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i)
+    {
+        if (!mark[i])
+        {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<bool> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
