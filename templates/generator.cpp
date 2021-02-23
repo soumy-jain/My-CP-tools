ad9edaf2dfd53835815365c7179879c79a7fc01b
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-02-23 19:42:38 +0530

    Update generator.cpp

--- templates/generator.cpp
+++ templates/generator.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
-#define ll        long long
-#define ull       unsigned long long
-#define SPEEDHACK ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define SPEEDHACK ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
-ll rand(int a, int b)
+mt19937_64 rng(12345);
+
+ll rand(ll a, ll b)
 {
-    return a + rand() % (b - a + 1);
+    return (ll)(rng() % (b - a + 1)) + a;
 }
 
 int main()
 {
-    SPEEDHACK //file
-    ll t,n; cin>>t>>n;
-    cout<<t<<'\n';
-    while(t--)
+    SPEEDHACK
+    // file
+    ll t, n;
+    cin >> t >> n;
+    cout << t << '\n';
+    while (t--)
     {
-        cout<<n<<'\n';
-        for(ll i=0;i<n;i++)
+        cout << n << '\n';
+        for (ll i = 0; i < n; i++)
         {
-            if((i&1))
-                cout<<-2;
-            else
-                cout<<rand(1,1000000000);
-            cout<<"\n "[i!=n-1];
+            cout << rand(1, 1000000);
+            cout << "\n "[i != n - 1];
         }
     }
     return 0;
