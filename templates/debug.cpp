becf6b76a0be912fc7ce2f23b9b0885c2f090cbd
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-04-18 21:19:58 +0530

    Update debug.cpp

--- templates/debug.cpp
+++ templates/debug.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace pb_ds; 
-//   
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace pb_ds;
+//
 // template<typename T>
 // using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 // clang-format off
@@ -30,15 +30,26 @@ void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 #ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
 #else
-#define debug(x...)
+#define dbg(x...)
 #endif
 // clang-format on
+
+void work()
+{
+    
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    ll t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        work();
+    }
     return 0;
 }
