03c2032f506c706cc38fe1de17af3249318f86d2
Soumy Jain <55257561+soumy-jain@users.noreply.github.com>
2021-05-18 00:23:05 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -2,28 +2,28 @@ vector<ll> parent, Rank;
 
 ll find(ll v)
 {
-	return parent[v] == v ? v : v = find(parent[v]);
+    return parent[v] == v ? v : parent[v] = find(parent[v]);
 }
 
 void merge(ll u, ll v)
 {
-	u = find(u);
-	v = find(v);
-	if (u == v)
-		return;
-	if (Rank[v] < Rank[u])
-		swap(v, u);
-	parent[u] = v;
-	Rank[v] += Rank[u];
+    u = find(u);
+    v = find(v);
+    if (u == v)
+        return;
+    if (Rank[v] < Rank[u])
+        swap(v, u);
+    parent[u] = v;
+    Rank[v] += Rank[u];
 }
 
 void DSU(ll N)
 {
-	parent.clear();
-	Rank.clear();
-	parent.resize(N);
-	Rank.resize(N, 1);
-	iota(parent.begin(), parent.end(), 0);
+    parent.clear();
+    Rank.clear();
+    parent.resize(N);
+    Rank.resize(N, 1);
+    iota(parent.begin(), parent.end(), 0);
 }
 
 DSU(n + 50);
